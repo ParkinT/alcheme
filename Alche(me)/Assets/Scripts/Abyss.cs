cfb88b0da1044e50ae523cf1cb292376b6902289
Fred Gohsman <fred.gohsman@agilethought.com>
2016-01-30 12:14:25 -0500

    Created new folder structure

---
+++ Alche(me)/Assets/Scripts/Abyss.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Abyss : MonoBehaviour
+{
+    /// <summary>
+    /// The point where the player will be placed when the level starts.
+    /// </summary>
+    public Transform startPoint;
+
+
+    /// <summary>
+    /// Handles another game object coming in contact with the Abyss.
+    /// If the other game object is the player, then the player is moved to the starting position.
+    /// </summary>
+    /// <param name="other">The game object that has collided with this game object.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = startPoint.position;
+        }
+    }
+}
